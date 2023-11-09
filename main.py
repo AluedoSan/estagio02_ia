6a4234cb4a28815c0f676086f39593864ce3dea1
Alexander Ueno <82620144+AluedoSan@users.noreply.github.com>
2023-11-09 17:30:23 -0300

    Feito banco de dados com o login e histórico

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, flash, redirect
 from src.algorithm import Algorithm as alg
-from sqlalchemy import create_engine, Column, Integer, String, delete
+from sqlalchemy import create_engine, Column, Integer, String, Boolean,delete, DATE, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 import plotly.express as px
 import pandas as pd
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from datetime import date
 
 #*BANCO DE DADOS
 engine = create_engine('mysql://root:root@localhost/estagio02')
 Base = declarative_base()
-class Usuario_BD(Base):
-    __tablename__ = 'users_bd'
+class Usuario_BD(Base, UserMixin):
+    __tablename__ = 'user_bd'
     id = Column(Integer, primary_key=True)
-    name = Column(String(255))
-    password = Column(String(25))
+    usuario = Column(String(255))
+    senha = Column(String(25))
     email = Column(String(25))
-    
+    admin = Column(Boolean)
+
 class Algoritimo_BD(Base):
     __tablename__ = 'algoritimo_bd'
     id = Column(Integer, primary_key=True)
@@ -27,48 +30,85 @@ class Algoritimo_BD(Base):
     temper = Column(Integer)
     umid = Column(Integer)
     resultado = Column(String(15))
-
+    data = Column(DATE)
+    user_id = Column(Integer, ForeignKey('user_bd.id'))
+    
+    user_relation = relationship('Usuario_BD')
 #Base.metadata.create_all(engine)
 #*Definição do app para utilizar o Flask
 app = Flask(__name__)
 app.secret_key = '@lG0r1t1m0-Agr0N0m1@'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-
+@login_manager.user_loader
+def load_user(user_id):
+    # Carregue o usuário com base no user_id do banco de dados
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    user = session.query(Usuario_BD).get(user_id)
+    session.close()
+    return user
 
 #! ROTA DE LOGIN
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    
+    if request.method == 'POST':
+        # Verifique as credenciais do usuário (substitua isso pela lógica real)
+        usuario = request.form['usuario']
+        senha = request.form['senha']
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        verify_user = session.query(Usuario_BD).filter_by(usuario=usuario).first()
+        session.close()
+        if verify_user and senha == verify_user.senha and verify_user.admin == True:
+            login_user(verify_user)
+            return redirect(url_for('register'))
+        elif verify_user and senha == verify_user.senha:
+            login_user(verify_user)
+            return redirect(url_for('algorithm_screen'))
+        else:
+            flash('Credenciais inválidas', 'error')
+
     return render_template('index.html')
+
 
 #! ROTA DE ADMIN
 @app.route('/admin', methods=['GET','POST'])
+@login_required
 def register():
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    all_users = session.query(Usuario_BD).all()
     if request.method == "POST":
         #*Pegando os dados e atribuindo a uma variável
         users=request.form.get('users')
         passwords=request.form.get('passwords')
         emails=request.form.get('emails')
-
-        users=str(users)
+        admin = request.form.get('admin')
+        
+        #*Arrumando o tipo das variáveis
+        users = str(users)
+        admin = bool(admin)
         passwords=str(passwords)
         emails=str(emails)
         
         #*Banco de dados
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        new_user = Usuario_BD(name=users, password=passwords, email=emails)
-        session.add(new_user)
+        
+        user_bd = Usuario_BD(email=emails, usuario=users, senha=passwords, admin=admin)
+        session.add(user_bd)
         session.commit()
         session.close()
-        return render_template('admin.html', users=users, emails=emails, passwords=passwords)
+        return render_template('admin.html', users=users, emails=emails)
     else:
-        return render_template('admin.html',users='', emails='', passwords='')
+        session.close()
+        return render_template('admin.html',users=all_users)
 
 
 
 #! ROTA DE HISTÓRICO
 @app.route('/historic')
+@login_required
 def historic():
     return render_template('historic.html')
 
@@ -79,15 +119,20 @@ def teste():
 
 #! ROTA DO ALGORITIMO
 @app.route('/algorithm')
+@login_required
 def algorithm_screen():
+    user_log = current_user.usuario
     return render_template("algorithm.html", PH = '', potassio = '', fosforo = ''
-                           , rainfall = '', nitrogenio = '', temp = '', water = '')
+                           , rainfall = '', nitrogenio = '', temp = '', water = '', user_log=user_log)
 
 #! ROTA DA FUNÇÃO DO ALGORITIMO
 @app.route('/calc', methods=['POST'])
+@login_required
 def algorithm_calc():
-    #*Conexão com o banco
+    user_log = current_user.usuario
+    user_id = current_user.id
     
+    #*Conexão com o banco
     #* Código para pegar os valores do usuário
     PH = request.form.get('PH')
     fosforo = request.form.get('fosforo')
@@ -97,6 +142,7 @@ def algorithm_calc():
     temp = request.form.get('temp')
     water = request.form.get('water')
 
+
     #* Código para transformar todos os valores em Float
     PH = float(PH)
     fosforo = float(fosforo)
@@ -105,6 +151,7 @@ def algorithm_calc():
     rainfall = float(rainfall)
     temp = float(temp)
     water = float(water)
+    data = date.today()
 
     #! Instância da minha classe
     instance_algorithm = alg(PH, fosforo, potassio, nitrogenio, rainfall, temp, water)
@@ -114,7 +161,7 @@ def algorithm_calc():
     Session = sessionmaker(bind=engine)
     session = Session()
     new_exec = Algoritimo_BD(ph=PH, p=fosforo, k=potassio, n=nitrogenio, chuva=rainfall, temper=temp,
-                            umid=water, resultado=result)
+                            umid=water, resultado=result, data=data, user_id = user_id)
     session.add(new_exec)
     session.commit()
     # Fechar a sessão
@@ -190,7 +237,8 @@ def algorithm_calc():
                            mean_potassio=mean_potassio,
                            std_dev_potassio=std_dev_potassio,
                            veracid=veracid,
-                           graphic = fig.to_html(full_html=False))
+                           graphic = fig.to_html(full_html=False),
+                           user_log=user_log)
 
 
 if __name__ == '__main__':
